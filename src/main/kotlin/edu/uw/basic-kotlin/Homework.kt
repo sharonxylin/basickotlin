ed257a0c98233e82366ac9b6361c4574899d29c3
sharonxylin <sl742041@gmail.com>
2022-01-11 00:01:56 -0800

    first and second section

--- src/main/kotlin/edu/uw/basic-kotlin/Homework.kt
+++ src/main/kotlin/edu/uw/basic-kotlin/Homework.kt
@@ -9,12 +9,30 @@ class Library {
 }
 
 // write a "whenFn" that takes an arg of type "Any" and returns a String
-
+whenFn (x: Any): String {
+  "Hello" -> return "world"
+  x!="Hello" -> return "I don't understand"
+  0 -> return "zero"
+  1 -> return "one"
+  in 2..10 -> return "low number"
+  !in 2..10 -> return "a number"
+  else -> return "I don't understand"
+}
+    
 // write an "add" function that takes two Ints, returns an Int, and adds the values
+fun add(int1: Int, int2: Int): Int {
+  return int1 + int2
+}
 
 // write a "sub" function that takes two Ints, returns an Int, and subtracts the values
+fun sub(int1: Int, int2: Int): Int {
+  return int1 - int2
+}
 
 // write a "mathOp" function that takes two Ints and a function (that takes two Ints and returns an Int), returns an Int, and applies the passed-in-function to the arguments
+fun mathOp(int1: Int, int2: Int, func: (Int, Int)-> Int): Int{
+    func(int1, int2)
+}
 
 // write a class "Person" with first name, last name and age
 
